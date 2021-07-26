55e0607683dd71033a3bae4671f2c963a633529d
Codetemplar99 <udoagwac@gmail.com>
2021-07-26 14:22:19 +0100

    remove default for sql

--- config/database.php
+++ config/database.php
@@ -18,8 +18,8 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION'),
-    // 'default' => env('DB_CONNECTION', 'pgsql'),
+    // 'default' => env('DB_CONNECTION',),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
