393cc46840ebc65bd230f5ad8f2ab53a96bb8dc3
Codetemplar99 <udoagwac@gmail.com>
2021-07-27 19:23:49 +0100

    comment files

---
+++ database/migrations/2021_07_27_181924_create_comments_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->integer('question_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            $table->text('comment');
+            $table->timestamps();
+        });
+        Schema::table('comments', function($table) {
+            $table->foreign('question_id')->references('id')->on('questions')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
